80ddd48d8e872ef31705a6c6e9f946b3811f5558
zamb <zamb@gmx.at>
2015-11-28 12:05:14 +0100

    First experimental version of model building and visualization of some example (TODO: fix size of shape variations...)

--- examples/training.cpp
+++ examples/training.cpp
@@ -21,9 +21,63 @@ along with AAM.  If not, see <http://www.gnu.org/licenses/>.
 #include <aam/aam.h>
 #include <aam/trainer.h>
 #include <aam/io.h>
+#include <aam/show.h>
+#include <aam/barycentrics.h>
+#include <aam/rasterization.h>
+#include <aam/map.h>
 
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+
+
+void printMat(aam::MatrixX m) {
+    for (int i = 0; i < m.rows(); i++) {
+        for (int j = 0; j < m.cols(); j++) {
+            std::cout << m(i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+void visualize(aam::ActiveAppearanceModel& model) {
+
+    aam::MatrixX trafo = model.shapeTransformToTrainingData;
+    //trafo(2, 0) -= 200;
+    //trafo(2, 1) -= 200;
+
+    aam::RowVectorX s = aam::transformShape(trafo, model.shapeMean);
+
+    cv::Mat colors = aam::toOpenCVHeader<aam::Scalar>(model.appearanceMean.transpose());
+    cv::Mat image = cv::Mat(640, 480, CV_8U);
+    aam::writeShapeImage(s, model.triangleIndices, model.barycentricSamplePositions, colors, image);
+
+    printMat(model.shapeModeWeights.rightCols(1));
+    printMat(model.shapeModes.bottomRows(1));
+
+    aam::Scalar w = 0.00005 * sqrt(model.shapeModeWeights.rightCols(1)(0, 0));
+
+    
+    aam::RowVectorX s2 = aam::transformShape(trafo, model.shapeMean + w * model.shapeModes.bottomRows(1));
+    std::cout << "rasterizeShape..." << std::endl;
+    aam::MatrixX barys = aam::rasterizeShape(s2, model.triangleIndices, 640, 480);
+
+    cv::Mat s2Texture;
+    cv::Mat image2 = cv::Mat(640, 480, CV_8U);
+    std::cout << "read shape image..." << std::endl;
+    // read texture image from mean shape image
+    aam::readShapeImage(s, model.triangleIndices, barys, image, s2Texture);
+    std::cout << "write shape image..." << std::endl;
+    aam::writeShapeImage(s2, model.triangleIndices, barys, s2Texture, image2);
+
+    aam::drawShapeLandmarks(image, s, cv::Scalar(0));
+    cv::imshow("img1", image);
+
+    aam::drawShapeLandmarks(image2, s2, cv::Scalar(0));
+    cv::imshow("img2", image2);
+    cv::waitKey(0);
+}
+
 
 /**
 
@@ -37,12 +91,24 @@ int main(int argc, char **argv)
         return 0;
     }
 
+    
     aam::TrainingSet trainingSet;
     aam::loadAsfTrainingSet(argv[1], trainingSet);
 
-    aam::Trainer trainer(trainingSet);
-    //trainer.train();
+    aam::Trainer::createTriangulation(trainingSet);
 
+    //aam::showTrainingSet(trainingSet);
+
+    aam::Trainer trainer(trainingSet);
+    aam::ActiveAppearanceModel model;
+    trainer.train(model);
+
+    model.save("test.model");
+    
+    //aam::ActiveAppearanceModel model;
+    //model.load("test.model");
+
+    visualize(model);
 
     //aam::showTrainingSet(trainingSet);
 
